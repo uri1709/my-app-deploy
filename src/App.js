7d4f43e8e7f1371b579649e6cc5ab8b296ad67e1
uri1709 <uri1709k@gmail.com>
2024-09-22 20:42:38 +0600

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from 'react';
+import { buttons } from './buttons';
+import { BUTTON_GROUP, NUM, OPERATOR } from './constants';
+import styles from './App.module.css';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>DEPLOY REACT-APP GITHUB</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
+export const App = () => {
+	const [operand1, setOperand1] = useState(NUM.ZERO);
+	const [operator, setOperator] = useState(OPERATOR.NONE);
+	const [operand2, setOperand2] = useState(NUM.NONE);
+	const [isResult, setIsResult] = useState(false);
 
-export default App;
+	const appState = {
+		operand1,
+		setOperand1,
+		operator,
+		setOperator,
+		operand2,
+		setOperand2,
+		isResult,
+		setIsResult,
+	};
+
+	const screenClassName = styles.screen + (isResult ? ' ' + styles.screenResult : '');
+	const output = operand1 + (operator && ' ' + operator) + (operand2 && ' ' + operand2);
+
+	return (
+		<div className={styles.app}>
+			<div className={screenClassName}>{output}</div>
+			<div className={styles.buttons}>
+				<div className={styles.buttonsGroupLeft}>
+					{buttons.map(({ id, name, group, handler }) =>
+						group === BUTTON_GROUP.LEFT ? (
+							<button
+								className={styles.button}
+								key={id}
+								onClick={() => handler(appState, name)}
+							>
+								{name}
+							</button>
+						) : null,
+					)}
+				</div>
+				<div className={styles.buttonsGroupRight}>
+					{buttons.map(({ id, name, group, handler }) =>
+						group === BUTTON_GROUP.RIGHT ? (
+							<button
+								className={styles.button}
+								key={id}
+								onClick={() => handler(appState)}
+							>
+								{name}
+							</button>
+						) : null,
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
